01fe063775e0ee913a001aca5020c7c470c03326
eragorn <filipflis@wp.pl>
2023-01-11 22:44:03 +0100

    refactor + first implementation of insertion sort

--- sortingAlgorithms/data.go
+++ sortingAlgorithms/data.go
@@ -1,10 +1,12 @@
-package sort
+package sorting_algorithms
 
 import (
 	"math/rand"
+	"time"
 )
 
 func NewData(length int) []int {
+	rand.Seed(time.Now().UnixNano())
 	result := make([]int, length)
 	elementsRange := length * 10
 	for i := 0; i < length; i++ {
