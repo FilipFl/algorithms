2e1e33656ec8e17bfa62035abb76ef42652cfffe
eragorn <filipflis@wp.pl>
2023-01-11 23:27:21 +0100

    uncomment tests

--- sortingAlgorithms/sorting_algorithms_test.go
+++ sortingAlgorithms/sorting_algorithms_test.go
@@ -1,7 +1,6 @@
 package sorting_algorithms
 
 import "testing"
-import "fmt"
 
 type insertSortTestData struct {
 	have []int 
@@ -9,18 +8,16 @@ type insertSortTestData struct {
 
 var insertTests = []insertSortTestData {
 	insertSortTestData{have: NewData(11)},
-	// insertSortTestData{have: NewData(100)},
-	// insertSortTestData{have: NewData(1000)},
-	// insertSortTestData{have: NewData(10000)},
-	// insertSortTestData{have: NewData(100000)},
+	insertSortTestData{have: NewData(100)},
+	insertSortTestData{have: NewData(1000)},
+	insertSortTestData{have: NewData(10000)},
+	insertSortTestData{have: NewData(100000)},
 }
 
 
 func TestInsertSort(t *testing.T) {
 	for _, test := range insertTests {
-		fmt.Println(test.have)
         sorted := InsertSort(test.have)
-		fmt.Println(sorted)
 		for i := 1; i < len(sorted); i++ {
 			if (sorted[i-1] > sorted[i]) {
 				t.Errorf("Array is not sorted properly")
