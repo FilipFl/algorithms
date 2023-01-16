4fe00e3c4431ed5c31e516a55fe4c2fe098c2014
eragorn <filipflis@wp.pl>
2023-01-16 19:09:14 +0100

    optimize algorithm, less operations

--- sortingAlgorithms/insertSort.go
+++ sortingAlgorithms/insertSort.go
@@ -5,11 +5,11 @@ func InsertSort(arr []int) []int {
 	for i := 1; i < len(arr); i++ {
 		val := arr[i]
 		j := i
-		for (j > 0 && arr[j] < arr[j-1]) {
+		for (j > 0 && val < arr[j-1]) {
 			arr[j] = arr[j-1]
 			j = j-1
-			arr[j] = val
 		}
+		arr[j] = val
 	}
 	return arr
 }
